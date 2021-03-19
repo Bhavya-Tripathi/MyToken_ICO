346675341b881508b220cbcaa1ac723b93eadb21
Bhavya Tripathi <bhavyatripathi00@gmail.com>
2021-03-19 13:05:47 +0530

    2. Transfer Tokens

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -6,11 +6,46 @@ contract MyToken{
     // Constructor: sets initial value of tokens
     // set the number of tokens
     // read the total number of tokens
-    uint public totalSupply =0;
-    constructor() public {
-        totalSupply = 10;
+
+    // name of token
+    string public name = 'My Token';
+    // symbol
+    string public symbol = 'MYT';
+    // standard
+    string public standard = 'My token v1.0';
+
+    uint public totalSupply;
+
+    // declaring a transfer event
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+
+    // who has each token
+    mapping(address => uint) public balanceOf;
+
+    constructor(uint _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        // allocate the initial supply
     }
     function Token () public view returns(uint)  {
        return totalSupply ;
+    }
+    // transfer function
+    
+    function transfer(address _to, uint _value) public returns(bool success){
+   
+    // throws excpetion if account doesn't have enough ether
+    // require in solidity does the following: if the statement evaluates to true then continue function execution else stop function execution and throw error
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+     // triggers a transfer event
+        emit Transfer(msg.sender, _to, _value);
+    // return a boolean
+        return true;
     }
 }
--- test/MyToken.js
+++ test/MyToken.js
@@ -1,15 +1,68 @@
 const MyToken = artifacts.require("MyToken");
 
 contract('MyToken', function(accounts){
-    it('sets the total supply upon deployment', function(){
+    var tokenInstance;
+    
+    // checking name of the token
+    it('initializes the contract with the correct values',function(){
+        return MyToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name){
+            assert.equal(name, 'My Token', 'has the correct name');
+            return tokenInstance.symbol();
+        }).then(function(symbol){
+            assert.equal(symbol, 'MYT', 'has the correct symbol');
+            return tokenInstance.standard();
+        }).then(function(standard){
+            assert.equal(standard, 'My token v1.0', 'has the correct standard');
+        })
+    })
+
+    it('allocates the initial supply upon deployment', function(){
         return MyToken.deployed().then(function(instance){
             tokenInstance = instance;
             return tokenInstance.totalSupply();
         }).then(function(totalSupply){
             assert.equal(totalSupply.toNumber(), 10, 'sets the total supply to 10');
-        })
-    })
-})
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(adminBalance){
+            assert.equal(adminBalance.toNumber(), 10, 'it allocates the initial supply to the admin');
+        });
+       
+    });
+    it('transfers token ownership ', function(){
+        return MyToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            // test 'require' statement first by transferring something larger than the sender's balance
+            return tokenInstance.transfer.call(accounts[1], 90 );
+       
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+            return tokenInstance.transfer.call(accounts[1], 2, {from: accounts[0]});
+            
+        }).then(function(success){
+            assert.equal(success, true, 'it returns true');
+            return tokenInstance.transfer(accounts[1], 2,  {from: accounts[0]});
+        }).then(function(receipt){
+            // event information present in logs
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+            assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+            assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
+            assert.equal(receipt.logs[0].args._value, 2, 'logs the transfer amount');
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 2, 'adds the amount to receiving account');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 8, 'deducts the amount from the sending account');
+        });
+    });
+
+});
+
+   
 
 
 
