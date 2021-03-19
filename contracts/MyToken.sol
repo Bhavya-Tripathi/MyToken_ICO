9f0d409791558a090b48d7e086322d9d94aa2992
Bhavya Tripathi <bhavyatripathi00@gmail.com>
2021-03-19 17:39:35 +0530

    3. Delegated Transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -23,29 +23,67 @@ contract MyToken{
         uint _value
     );
 
+    // approve event
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
     // who has each token
     mapping(address => uint) public balanceOf;
+
+    // allowance
+    // account A approves Account B to spend value, hence nested mappings
+    mapping(address => mapping(address => uint)) public allowance;
 
     constructor(uint _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
         // allocate the initial supply
     }
-    function Token () public view returns(uint)  {
-       return totalSupply ;
-    }
+  
     // transfer function
     
     function transfer(address _to, uint _value) public returns(bool success){
    
-    // throws excpetion if account doesn't have enough ether
+    // throws excpetion if account doesn't have enough tokens
     // require in solidity does the following: if the statement evaluates to true then continue function execution else stop function execution and throw error
-        require(balanceOf[msg.sender] >= _value);
+        require(balanceOf[msg.sender] >= _value, "Not enough balance");
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
      // triggers a transfer event
         emit Transfer(msg.sender, _to, _value);
     // return a boolean
+        return true;
+    }
+
+    // Delegated Transfer
+
+    // approve
+    function approve(address _spender, uint _value) public returns(bool success) {
+        // allowance
+        allowance[msg.sender][_spender] = _value;
+        // approvement
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // transferred From (transfer on behalf of other party)
+    // three accounts, A -> calls the function, B -> account transferring the money, C-> moneny being transferred to
+    function transferFrom(address _from, address _to, uint _value) public returns(bool success){
+        // require _from has enough tokens
+        require(_value <= balanceOf[_from], "Not enough money in spending account");
+        // require allowance is big enough
+        require(_value <= allowance[_from][msg.sender], "Value larger than approved amount");
+        // call transfer event
+        // change balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        // update allowance
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
